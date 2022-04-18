a56a702b8749ee15dec91cad8eff15b21882deec
Philip <philip_angelin@hotmail.com>
2022-04-18 15:37:38 +0200

    feat(useIsOnline): wrap in browser

--- src/components/demo/UseIsOnlineDemo.tsx
+++ src/components/demo/UseIsOnlineDemo.tsx
@@ -1,10 +1,11 @@
 import { useIsOnline } from "use-ful-hooks-ts";
 import React from "react";
 import { withThemeProvider } from "../withThemeProvider";
+import { withBrowserOnly } from "../withBrowserOnly";
 
 function UseIsOnlineDemo() {
 	const isOnline = useIsOnline();
 	return <pre>{String(isOnline)}</pre>;
 }
 
-export default withThemeProvider(UseIsOnlineDemo);
+export default withThemeProvider(withBrowserOnly(UseIsOnlineDemo));
